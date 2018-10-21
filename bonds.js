eb09b08aec37452210cb2916ea7f267207925401
James Arnall <james@jamesarnall.com>
2018-10-21 16:26:02 -0500

    Bootstrap styling, data retrieval, jQuery, Gulp, Chosen select control

---
+++ bonds.js
@@ -0,0 +1,12 @@
+bonds = bonds || [
+                    { actor : "Sean Connery",    id : "connery"  },
+                    { actor : "David Niven",     id : "niven"    },
+                    { actor : "George Lazenby",  id : "lazenby"  },
+                    { actor : "Roger Moore",     id : "moore"    },
+                    { actor : "Timothy Dalton",  id : "dalton"   },
+                    { actor : "Pierce Brosnan",  id : "brosnan"  },
+                    { actor : "Daniel Craig",    id : "craig"    },
+                    { actor : "Peter Sellers",   id : "sellers"  },
+                    { actor : "Woody Allen",     id : "allen"    },
+                    { actor : "Barry Nelson",    id : "nelson"   }
+                 ];
